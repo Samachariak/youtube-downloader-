ba8e62621b144f37f3d6098b0c3db1cc6f60ea81
Samachariak <samuelkamau298@gmail.com>
2022-07-08 11:31:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import filedialog
+from turtle import title
+
+screen = Tk()
+title = screen.title('youtube download')
+canvas = Canvas(screen, width=500, height=500)
+canvas.pack()
+
+
+
+screen.mainloop()
